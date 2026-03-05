3b4ead16ac7c2626d6b523a5f5768b1bb76e2c10
1310020 <kevinromerohernandez16@gmail.com>
2026-03-05 14:35:16 -0500

    Started Dieting app

--- app/src/main/java/org/baltimorecityschools/dietingapp/MainActivity.java
+++ app/src/main/java/org/baltimorecityschools/dietingapp/MainActivity.java
@@ -1,6 +1,9 @@
 package org.baltimorecityschools.dietingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button startButton, settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.startButton);
+        settingsButton = findViewById(R.id.settingsButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent theIntent = new Intent(MainActivity.this, SettingsActivity.class);
+
+                startActivity(theIntent);
+            }
+        });
 
     }
 }
