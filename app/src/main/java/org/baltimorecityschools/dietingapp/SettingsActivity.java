d245ae814da7d5ad548ee860e36d99e4f43bbf29
OWNER <kevinromerohernandez16@gmail.com>
2026-03-05 18:36:36 -0800

    Made it so that you can go back and forth between the settings and the main screen

--- app/src/main/java/org/baltimorecityschools/dietingapp/SettingsActivity.java
+++ app/src/main/java/org/baltimorecityschools/dietingapp/SettingsActivity.java
@@ -1,6 +1,6 @@
 package org.baltimorecityschools.dietingapp;
 
-import android.os.Bundle;
+import android.content.Intent;import android.os.Bundle;import android.view.View;import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +9,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+    Button backroundButton, fontButton, languageButton, settingsMainMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        backroundButton = findViewById(R.id.backroundButton);
+        fontButton = findViewById(R.id.fontButton);
+        languageButton = findViewById(R.id.languageButton);
+        settingsMainMenuButton = findViewById(R.id.settingsMainMenuButton);
+
+
+
+        settingsMainMenuButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View view) {
+                Intent settingMainIntent = new Intent(SettingsActivity.this, MainActivity.class);
+
+
+                startActivity(settingMainIntent);
+            }});
     }
 }
