e7a13d0010a0275aaf2e0f1014cc630c73f61965
1310020 <kevinromerohernandez16@gmail.com>
2026-03-16 12:41:47 -0400

    Added diet recipe class to app

--- app/src/main/java/org/baltimorecityschools/dietingapp/DietList.java
+++ app/src/main/java/org/baltimorecityschools/dietingapp/DietList.java
@@ -12,7 +12,45 @@ public class DietList {
         recipeName = "";
         recipeCost = 0.00;
         calorieCount = 0;
-        
+        dietRecipeIngredients = new ArrayList<String>();
     }
 
+    public DietList(String recipeName, double recipeCost, int calorieCount, ArrayList<String> dietRecipeIngredients) {
+        this.recipeName = recipeName;
+        this.recipeCost = recipeCost;
+        this.calorieCount = calorieCount;
+        this.dietRecipeIngredients = dietRecipeIngredients;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public double getRecipeCost() {
+        return recipeCost;
+    }
+
+    public void setRecipeCost(double recipeCost) {
+        this.recipeCost = recipeCost;
+    }
+
+    public int getCalorieCount() {
+        return calorieCount;
+    }
+
+    public void setCalorieCount(int calorieCount) {
+        this.calorieCount = calorieCount;
+    }
+
+    public ArrayList<String> getDietRecipeIngredients() {
+        return dietRecipeIngredients;
+    }
+
+    public void setDietRecipeIngredients(ArrayList<String> dietRecipeIngredients) {
+        this.dietRecipeIngredients = dietRecipeIngredients;
+    }
 }
