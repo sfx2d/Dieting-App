ef42fc7388ef64c49fba980067e52c3d80cfbc8b
1310020 <kevinromerohernandez16@gmail.com>
2026-05-13 14:31:11 -0400

    It displays information for ALL foods. Reminder to not set the index number pulling a non existent item(element). (Ex: Only 3 items in object list, and you set the index to 5, but since there is no item at index '5', it is not able to pull anything thus crashing.)

--- app/src/main/java/org/baltimorecityschools/dietingapp/dietSearch.java
+++ app/src/main/java/org/baltimorecityschools/dietingapp/dietSearch.java
@@ -35,56 +35,32 @@ public class dietSearch extends AppCompatActivity {
         lentilTacoFillingMCV = findViewById(R.id.lentilTacoFillingMCV);
 
 
-        ArrayList<String>recipeIngredients = new ArrayList<String>();
+
 
         //Below each comment are what foods' ingredients it is referring to.
 
         //AppleSauce French Toast ingredients
-        recipeIngredients.add(0, "2 eggs, 1/2 cup nonfat or 1% milk, 1 teaspoon cinnamon, 2 teaspoons sugar, 1/2 teaspoon vanilla, 1/4 cup unsweetened applesauce, 6 slices whole-wheat bread");
+
         //Any Berry Sauce ingredients
-        recipeIngredients.add(1,"1/4 cup cold water, 1 Tablespoon cornstarch, 1/3 cup sugar 4 cups berries, fresh or frozen (blackberries, raspberries, blueberries, sliced strawberries, or a mixture)");
+
         //BreakFast Burritos ingredients
-        recipeIngredients.add(2,"4 eggs, 1/4 cup nonfat or 1% milk, salt, pepper and chili powder to taste, 1 teaspoon vegetable oil, 4 (10-inch) flour tortillas, 1 cup fat-free refried beans, 1/2 cup (2 ounces) grated cheddar cheese, 1 tomato, chopped");
+
         //Oven Baked Salmon ingredients
-        recipeIngredients.add(3,"1 Tablespoon vegetable oil, 12 to 16 ounces fresh or thawed salmon, cut into 3 or 4 pieces of similar size, 1/4 teaspoon salt, 1/4 teaspoon pepper");
+
         //Baked Meat Balls ingredients
-        recipeIngredients.add(4,"1/2 cup finely grated carrot, 1 egg, 1/2 cup oats (old fashioned or quick-cooking), 1/4 cup skim or 1% milk, 1/2 teaspoon salt, 1/2 teaspoon pepper, 1/2 teaspoon garlic powder, 1/2 teaspoon onion powder, 1/2 teaspoon oregano, 1 pound lean ground meat (any type, 15% fat or less)");
+
         //Cheesy Beef Pasta ingredients
-        recipeIngredients.add(5,"/2 pound lean ground beef (15% fat)\n" +
-                "1 onion, diced (about 1 cup)\n" +
-                "2 cloves garlic, minced, or 1/2 teaspoon garlic powder\n" +
-                "1 small zucchini, chopped (about 1 1/2 cups)\n" +
-                "1 jar (24 to 26 ounces) tomato-based pasta sauce\n" +
-                "1/2 teaspoon dried basil\n" +
-                "1/2teaspoon dried oregano\n" +
-                "1/4 teaspoon red pepper flakes (optional)\n" +
-                "12 ounces rotini pasta (about 4 cups)\n" +
-                "1/2 cup (2 ounces) shredded cheddar cheese\n" +
-                "1 1/2 cups (6 ounces) shredded mozzarella cheese");
+
         //Fiesta Barley Salad ingredients
-        recipeIngredients.add(6,"1 cup pearl barley*\n" +
-                "3 cups water\n" +
-                "1/4 cup raisins, or other dried fruit\n" +
-                "1 cup frozen peas, or other vegetables (fresh, frozen or canned and drained)\n" +
-                "3 cups chopped lettuce\n" +
-                "1 can (15 ounces) mandarin oranges, drained\n" +
-                "1/2 cup sliced green onion (try any type of onion)\n" +
-                "1 Tablespoon vinegar (rice vinegar or any others)\n" +
-                "3 Tablespoons vegetable oil");
+
         //Lentil Taco Filling
-        recipeIngredients.add(7,"1/2 cup chopped onion\n" +
-                "1/2 cup chopped bell pepper (any color)\n" +
-                "1/4 teaspoon garlic powder or 1 clove garlic, minced\n" +
-                "1 teaspoon oil\n" +
-                "1/2 cup dried lentils, rinsed\n" +
-                "4 1/2 teaspoons chili powder\n" +
-                "1 teaspoon ground cumin\n" +
-                "1/2 teaspoon dried cilantro or oregano (optional)\n" +
-                "1 1/4 cups broth, any flavor");
+
 
         appleSauceFrenchToastMCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ArrayList<String>recipeIngredients = new ArrayList<String>();
+                recipeIngredients.add("2 eggs, 1/2 cup nonfat or 1% milk, 1 teaspoon cinnamon, 2 teaspoons sugar, 1/2 teaspoon vanilla, 1/4 cup unsweetened applesauce, 6 slices whole-wheat bread");
                 Intent reusableInfoDisplay = new Intent(dietSearch.this, ReusableInfoDisplay.class);
                 reusableInfoDisplay.putExtra("TEXT_DATA",recipeIngredients.get(0));
                 DietList newDietList = new DietList("AppleSauce French Toast ", 5.32, 140, recipeIngredients);
@@ -100,8 +76,10 @@ public class dietSearch extends AppCompatActivity {
         anyBerrySauceMCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ArrayList<String>recipeIngredients = new ArrayList<String>();
+                recipeIngredients.add(0,"1/4 cup cold water, 1 Tablespoon cornstarch, 1/3 cup sugar 4 cups berries, fresh or frozen (blackberries, raspberries, blueberries, sliced strawberries, or a mixture)");
                 Intent reusableInfoDisplay = new Intent(dietSearch.this, ReusableInfoDisplay.class);
-                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(1));
+                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(0));
                 DietList newDietList = new DietList("Any Berry Sauce ", 4.92, 60, recipeIngredients);
                 String recipe = ("" + newDietList.getRecipeName() + "\n" + "$" + newDietList.getRecipeCost() + "\n" + "Calories: " + newDietList.calorieCount + "\n" + "Ingredient Info: " + newDietList.getDietRecipeIngredients());
                 reusableInfoDisplay.putExtra("TEXT", recipe);
@@ -113,8 +91,10 @@ public class dietSearch extends AppCompatActivity {
         breakFastBurritosMCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ArrayList<String>recipeIngredients = new ArrayList<String>();
+                recipeIngredients.add(0,"4 eggs, 1/4 cup nonfat or 1% milk, salt, pepper and chili powder to taste, 1 teaspoon vegetable oil, 4 (10-inch) flour tortillas, 1 cup fat-free refried beans, 1/2 cup (2 ounces) grated cheddar cheese, 1 tomato, chopped");
                 Intent reusableInfoDisplay = new Intent(dietSearch.this, ReusableInfoDisplay.class);
-                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(2));
+                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(0));
                 DietList newDietList = new DietList("Breakfast Burritos ", 5.67, 210, recipeIngredients);
                 String recipe = ("" + newDietList.getRecipeName() + "\n" + "$" + newDietList.getRecipeCost() + "\n" + "Calories: " + newDietList.calorieCount + "\n" + "Ingredient Info: " + newDietList.getDietRecipeIngredients());
                 reusableInfoDisplay.putExtra("TEXT", recipe);
@@ -128,7 +108,9 @@ public class dietSearch extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent reusableInfoDisplay = new Intent(dietSearch.this, ReusableInfoDisplay.class);
-                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(3));
+                ArrayList<String>recipeIngredients = new ArrayList<String>();
+                recipeIngredients.add(0,"1 Tablespoon vegetable oil, 12 to 16 ounces fresh or thawed salmon, cut into 3 or 4 pieces of similar size, 1/4 teaspoon salt, 1/4 teaspoon pepper");
+                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(0));
                 DietList newDietList = new DietList("Oven Baked Salmon ", 4.56, 160, recipeIngredients);
                 String recipe = ("" + newDietList.getRecipeName() + "\n" + "$" + newDietList.getRecipeCost() + "\n" + "Calories: " + newDietList.calorieCount + "\n" + "Ingredient Info: " + newDietList.getDietRecipeIngredients());
                 reusableInfoDisplay.putExtra("TEXT", recipe);
@@ -141,8 +123,10 @@ public class dietSearch extends AppCompatActivity {
         bakedMeatBallsMCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ArrayList<String>recipeIngredients = new ArrayList<String>();
+                recipeIngredients.add(0,"1/2 cup finely grated carrot, 1 egg, 1/2 cup oats (old fashioned or quick-cooking), 1/4 cup skim or 1% milk, 1/2 teaspoon salt, 1/2 teaspoon pepper, 1/2 teaspoon garlic powder, 1/2 teaspoon onion powder, 1/2 teaspoon oregano, 1 pound lean ground meat (any type, 15% fat or less)");
                 Intent reusableInfoDisplay = new Intent(dietSearch.this, ReusableInfoDisplay.class);
-                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(4));
+                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(0));
                 DietList newDietList = new DietList("Baked Meatballs ", 4.30, 150, recipeIngredients);
                 String recipe = ("" + newDietList.getRecipeName() + "\n" + "$" + newDietList.getRecipeCost() + "\n" + "Calories: " + newDietList.calorieCount + "\n" + "Ingredient Info: " + newDietList.getDietRecipeIngredients());
                 reusableInfoDisplay.putExtra("TEXT", recipe);
@@ -155,8 +139,20 @@ public class dietSearch extends AppCompatActivity {
         cheesyBeefPastaMCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ArrayList<String>recipeIngredients = new ArrayList<String>();
+                recipeIngredients.add(0,"1/2 pound lean ground beef (15% fat)\n" +
+                        "1 onion, diced (about 1 cup)\n" +
+                        "2 cloves garlic, minced, or 1/2 teaspoon garlic powder\n" +
+                        "1 small zucchini, chopped (about 1 1/2 cups)\n" +
+                        "1 jar (24 to 26 ounces) tomato-based pasta sauce\n" +
+                        "1/2 teaspoon dried basil\n" +
+                        "1/2teaspoon dried oregano\n" +
+                        "1/4 teaspoon red pepper flakes (optional)\n" +
+                        "12 ounces rotini pasta (about 4 cups)\n" +
+                        "1/2 cup (2 ounces) shredded cheddar cheese\n" +
+                        "1 1/2 cups (6 ounces) shredded mozzarella cheese");
                 Intent reusableInfoDisplay = new Intent(dietSearch.this, ReusableInfoDisplay.class);
-                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(5));
+                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(0));
                 DietList newDietList = new DietList("Cheesy Beef Pasta ", 4.99, 380, recipeIngredients);
                 String recipe = ("" + newDietList.getRecipeName() + "\n" + "$" + newDietList.getRecipeCost() + "\n" + "Calories: " + newDietList.calorieCount + "\n" + "Ingredient Info: " + newDietList.getDietRecipeIngredients());
                 reusableInfoDisplay.putExtra("TEXT", recipe);
@@ -169,8 +165,18 @@ public class dietSearch extends AppCompatActivity {
         fiestaBarleySaladMCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ArrayList<String>recipeIngredients = new ArrayList<String>();
+                recipeIngredients.add(0,"1 cup pearl barley*\n" +
+                        "3 cups water\n" +
+                        "1/4 cup raisins, or other dried fruit\n" +
+                        "1 cup frozen peas, or other vegetables (fresh, frozen or canned and drained)\n" +
+                        "3 cups chopped lettuce\n" +
+                        "1 can (15 ounces) mandarin oranges, drained\n" +
+                        "1/2 cup sliced green onion (try any type of onion)\n" +
+                        "1 Tablespoon vinegar (rice vinegar or any others)\n" +
+                        "3 Tablespoons vegetable oil");
                 Intent reusableInfoDisplay = new Intent(dietSearch.this, ReusableInfoDisplay.class);
-                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(6));
+                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(0));
                 DietList newDietList = new DietList("Fiesta Barley Salad ", 5.11, 150, recipeIngredients);
                 String recipe = ("" + newDietList.getRecipeName() + "\n" + "$" + newDietList.getRecipeCost() + "\n" + "Calories: " + newDietList.calorieCount + "\n" + "Ingredient Info: " + newDietList.getDietRecipeIngredients());
                 reusableInfoDisplay.putExtra("TEXT", recipe);
@@ -183,8 +189,18 @@ public class dietSearch extends AppCompatActivity {
         lentilTacoFillingMCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ArrayList<String>recipeIngredients = new ArrayList<String>();
+                recipeIngredients.add(0,"1/2 cup chopped onion\n" +
+                        "1/2 cup chopped bell pepper (any color)\n" +
+                        "1/4 teaspoon garlic powder or 1 clove garlic, minced\n" +
+                        "1 teaspoon oil\n" +
+                        "1/2 cup dried lentils, rinsed\n" +
+                        "4 1/2 teaspoons chili powder\n" +
+                        "1 teaspoon ground cumin\n" +
+                        "1/2 teaspoon dried cilantro or oregano (optional)\n" +
+                        "1 1/4 cups broth, any flavor");
                 Intent reusableInfoDisplay = new Intent(dietSearch.this, ReusableInfoDisplay.class);
-                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(7));
+                reusableInfoDisplay.putExtra("TEXT_DATA", recipeIngredients.get(0));
                 DietList newDietList = new DietList("Lentil Taco Filling ", 4.54, 60, recipeIngredients);
                 String recipe = ("" + newDietList.getRecipeName() + "\n" + "$" + newDietList.getRecipeCost() + "\n" + "Calories: " + newDietList.calorieCount + "\n" + "Ingredient Info: " + newDietList.getDietRecipeIngredients());
                 reusableInfoDisplay.putExtra("TEXT", recipe);
